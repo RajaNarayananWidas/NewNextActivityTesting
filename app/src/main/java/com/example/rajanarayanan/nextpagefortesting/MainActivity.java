e9b8d5f34da2ac2b273c970c09fa4750304a535d
rajanarayanan <raja.narayanan@widas.in>
2018-09-05 15:57:23 +0530

    Git message

--- app/src/main/java/com/example/rajanarayanan/nextpagefortesting/MainActivity.java
+++ app/src/main/java/com/example/rajanarayanan/nextpagefortesting/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void nextActivity(View view){
+        Toast.makeText(this, "Next activvity", Toast.LENGTH_SHORT).show();
         Intent intent=new Intent(this,Main2Activity.class);
         startActivity(intent);
     }
